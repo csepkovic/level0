a1ecf197041fa969395af710a6dbdd4c70f770e7
Colin Sepkovic <Guest@wts5.attlocal.net>
2015-09-24 18:33:51 -0700

    hi

---
+++ src/airline_registration.java
@@ -0,0 +1,38 @@
+package level_0;
+
+//Copyright Wintriss Technical Schools 2013
+import javax.swing.JOptionPane;
+
+/**
+* Airline registration 
+* 
+* You are writing a program for a new airline to take their registration information at the check-in counter
+* and print their boarding pass. You need to collect the following information: 
+* 		First name 
+* 		Last name
+* 		Destination airport 
+* 		Birthday (for security purposes) 
+* 		Male/female (for security purposes) 
+* 
+* Print the boarding pass in this format:
+* LAST NAME / FIRST NAME (BIRTHDAY, M/F) 
+* Traveling to: DESTINATION
+* 
+* Toolbox: main() method, JOptionPane.showInputDialog, variables
+* 
+**/
+public class airline_registration {
+	public static void main(String[] args) {
+		String name= JOptionPane.showInputDialog("First Name");
+		String last= JOptionPane.showInputDialog("Last Name");
+		String destination= JOptionPane.showInputDialog("Destination Airport");
+		String bday= JOptionPane.showInputDialog("Birthday (for security purposes)");
+		String gender= JOptionPane.showInputDialog("Male/Female");
+		JOptionPane.showMessageDialog(null, last+"/"+name+"("+bday+", "+gender+") \n"+"Travelling to:"+destination);
+		System.out.println(last+ name+"("+bday+gender+")");
+		System.out.println("Travelling to: "+ destination);
+	}
+}
+
+
+
---
+++ src/programthing.java
@@ -0,0 +1,12 @@
+package level_0;
+
+import javax.swing.JOptionPane;
+
+public class programthing {
+	public static void main(String[] args) {
+		
+	
+String bruh=JOptionPane.showInputDialog("If Johnny has 250 brownies and eats all of them, what does she have left?");
+JOptionPane.showMessageDialog(null, "The answer is "+bruh);
+}
+}
